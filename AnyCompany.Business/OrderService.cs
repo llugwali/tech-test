e3b2f6a22e0a39ca4cc3fc70b8f871270ae1ed23
Lulamile <llugwali@gmail.com>
2020-08-14 22:03:12 +0200

    Save changes

---
+++ AnyCompany.Business/OrderService.cs
@@ -0,0 +1,39 @@
+﻿using AnyCompany.Business.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnyCompany.Models;
+using AnyCompany.Data.Contracts;
+
+namespace AnyCompany.Business
+{
+    public class OrderService : IOrderService
+    {
+        private IOrderRepository _orderRepository;
+        private ICustomerRepository _customerRepository;
+        public OrderService(ICustomerRepository customerRepository, IOrderRepository orderRepository)
+        {
+            _customerRepository = customerRepository;
+            _orderRepository = orderRepository;
+
+        }
+        public bool PlaceOrder(Order order, int customerId)
+        {
+            Customer customer = _customerRepository.Load(customerId);
+
+            if (order.Amount == 0)
+                return false;
+
+            if (customer.Country == "UK")
+                order.VAT = 0.2d;
+            else
+                order.VAT = 0;
+
+            _orderRepository.Save(order);
+
+            return true;
+        }
+    }
+}
